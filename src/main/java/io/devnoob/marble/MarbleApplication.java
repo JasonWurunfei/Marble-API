0f0fa1393ecb822c5d3d61c3e137bc615ff1c424
JasonWu <47469275+JasonWurunfei@users.noreply.github.com>
2021-06-16 21:17:36 +0800

    first commit

---
+++ src/main/java/io/devnoob/marble/MarbleApplication.java
@@ -0,0 +1,13 @@
+package io.devnoob.marble;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarbleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarbleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/devnoob/marble/MarbleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.devnoob.marble;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarbleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
