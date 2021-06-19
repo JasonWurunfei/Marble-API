557811ee21b5ab2c64a317a9dc2e8aaa3e3ed242
JasonWu <47469275+JasonWurunfei@users.noreply.github.com>
2021-06-19 15:47:46 +0800

    fix getUser API tests

--- src/test/java/io/devnoob/marble/api/UserControllerTest.java
+++ src/test/java/io/devnoob/marble/api/UserControllerTest.java
@@ -12,11 +12,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
-
 
 import io.devnoob.marble.persistence.entity.User;
 import io.devnoob.marble.persistence.repo.UserRepository;
@@ -59,16 +59,28 @@ public class UserControllerTest {
 
         @Test
         @DisplayName("Should return 404 when user does not exist")
-        void testGetUser() throws Exception {
+        void testGetUserShouldReturn404WhenUserDoesNotExist() throws Exception {
             Mockito.when(userRepository.find(2L)).thenReturn(null);
     
             String url = "/api/user/2";
             mockMvc.perform(get(url)).andExpect(status().isNotFound());
         }
 
-    }
+        @Test
+        @DisplayName("GetUser shoud return correct response body")
+        void testGetUserReturnCorrectResponseBody() throws Exception {
+            User user = new User(1L, "test_user1");
+            Mockito.when(userRepository.find(1L))
+                .thenReturn(user);
+    
+            String url = "/api/user/1";
+            MvcResult mvcResult = mockMvc.perform(get(url)).andReturn();
+            String actualJsonResponse = mvcResult.getResponse().getContentAsString();
+            String expectedJsonResponse = objectMapper.writeValueAsString(user);
+            assertEquals(expectedJsonResponse, actualJsonResponse);
+        }
 
-   
+    }
 
     @Test
     void testUpdateUser() {
